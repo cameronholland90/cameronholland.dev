0fa1ce63b54bbf07f2f03dfefee0ead7642d0692
Cameron Holland <cameron@thenearsky.com>
2015-10-07 11:48:22 -0500

    fixed blackjack view issue caused by local repo change

--- app/views/projects/blackjack.blade.php
+++ app/views/projects/blackjack.blade.php
@@ -1,107 +1,73 @@
 @extends('layouts.master')
 
+@section('tab-title')
+	<title>Projects - Blackjack</title>
+	<link href='https://fonts.googleapis.com/css?family=Black+Ops+One' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="/css/blackjack.css">
+@stop
+
 @section('content')
-	<?php
-
-	require_once('classes/blackjack-classes.php');
-
-	
-	if (isset($_SERVER['HTTP_REFERER']) && $_SERVER['HTTP_REFERER'] != 'http://cameronholland.me/blackjack') {
-		Session::forget('deck');
-		Session::forget('player');
-		Session::forget('dealer');
-		Session::forget('win');
-	} elseif (isset($_POST['restart']) || ((isset($_POST['playagain']) && Session::get('deck')->fullDeck) < 10))) {
-		Session::forget('deck');
-		Session::forget('player');
-		Session::forget('dealer');
-		Session::forget('win');
-	} elseif (isset($_POST['playagain'])) {
-		Session::forget('player');
-		Session::forget('dealer');
-		Session::forget('win');
-	}
-
-	if (!isset($_SESSION['deck'])) {
-		$_SESSION['deck'] = new Deck();
-		foreach ($_SESSION['deck']->suits as $suit) {
-			foreach ($_SESSION['deck']->cards as $card) {
-				$_SESSION['deck']->fullDeck[] = new Card($card, $suit);
-			}
-		}
-		$_SESSION['deck']->shuffleDeck();
-	}
-
-	if (!isset($_SESSION['player']) && !isset($_SESSION['dealer'])) {
-		$_SESSION['player'] = new Hand();
-		$_SESSION['dealer'] = new Hand();
-	}
-
-	if (!isset($_POST['stay'])) {
-		if (isset($_POST['hit']) && $_POST['hit'] === 'yes') {
-			$_SESSION['deck']->drawCard($_SESSION['player']);
-		}
-	} else {
-		while ($_SESSION['dealer']->getScore() < 17 && ($_SESSION['dealer']->getScore() < $_SESSION['player']->getScore())) {
-			$_SESSION['deck']->drawCard($_SESSION['dealer']);
-		}
-	}
-
-	?>
-
-	<div class='container main-container'>
-		<div class="page-header">
-			<h1>Blackjack <small>Coded by Cameron Holland</small></h1>
+	<div class="page-header">
+		<h1>Blackjack <small>Coded by Cameron Holland</small></h1>
+		<button id="rules" class="btn btn-default" data-bootboxalert="blackjack-rules">
+			<i class="fa fa-info-circle"></i> Rules
+		</button>
+		<button class="btn btn-success" id="change-card-back">Change Card Back</button>
+	</div>
+	<div class="row" id="record">
+		<div class="col-md-3 col-md-offset-1 text-center wins-loses">
+			<h2>Wins</h2>
+			<p id="wins-count">0</p>
 		</div>
-		<?php $hide = TRUE; ?>
-		<?php if ((($_SESSION['player']->getScore() === 21 || $_SESSION['player']->getScore() > 21) || isset($_POST['stay'])) || (isset($_SESSION['win']))) { ?>
-			<?php $hide = FALSE; ?>
-		<?php } ?>
-		<div class='row'>
-			<div class='col-md-4'>
-				<h3>Player Hand <?= "Score: " . $_SESSION['player']->getScore(); ?></h3>
-				<?php $_SESSION['player']->displayHand(); ?>
+		<div class="col-md-3 col-md-offset-4 text-center wins-loses">
+			<h2>Loses</h2>
+			<p id="loses-count">0</p>
+		</div>
+	</div>
+	<div class='row' id="game-board">
+		<div class='col-md-4'>
+			<div class="row">
+				<h3>Player Hand Score: <span id="score-1">0</span></h3>
+				<div id="hand-1" class="row"></div>
+				<div id="hand-options" class="row">
+					<button class="btn btn-success hit" data-handkey="1">Hit</button>
+					<button class="btn btn-danger stay" data-handkey="1">Stay</button>
+					<br>
+					<button class="split btn btn-primary" data-handkey="1">Split</button>
+				</div>
 			</div>
-			<div class='col-md-4'>
-				<h3>Dealer Hand <?= "Score: " . $_SESSION['dealer']->getScore($hide); ?></h3>
-				<?php $_SESSION['dealer']->displayHand($hide); ?>
-			</div>
-			<div class="col-md-4">
-				<h3>Deck <?= "Card Count: " . count($_SESSION['deck']->fullDeck); ?></h3>
-				<?php $_SESSION['deck']->displayDeck(); ?>
+			<div class="row" id="extra-hands">
+				<h3>Player Hand Score: <span id="score-2">0</span></h3>
+				<div id="hand-2" class="row"></div>
+				<div id="hand-options" class="row">
+					<button class="btn btn-success hit" data-handkey="2">Hit</button>
+					<button class="btn btn-danger stay" data-handkey="2">Stay</button>
+					<br>
+					<button class="split btn btn-primary" data-handkey="2">Split</button>
+				</div>
 			</div>
 		</div>
-		<div class='row'>
-			<div class='userstuff'>
-				<?php if ((($_SESSION['player']->getScore() === 21 || $_SESSION['player']->getScore() > 21) || isset($_POST['stay'])) || (isset($_SESSION['win']))) { ?>
-					<form method="POST" action="">
-						<button class='btn-md btn-success' name="playagain" type="submit" value="yes" autofocus="autofocus">Play Again</button>
-						<button class='btn-md btn-danger' name="restart" type="submit" value="yes">Restart Game</button>
-					</form>
-					<?php if ($_SESSION['dealer']->getScore() > 21) { ?>
-						<h3>Player Wins! Dealer Busted!</h3>
-						<?php $_SESSION['win'] = TRUE; ?>
-					<?php } elseif ($_SESSION['player']->getScore() > 21) { ?>
-						<h3>Dealer Wins! Player Busted!</h3>
-						<?php $_SESSION['win'] = TRUE; ?>
-					<?php } elseif ($_SESSION['dealer']->getScore() > $_SESSION['player']->getScore()) { ?>
-						<h3>Dealer Wins!</h3>
-						<?php $_SESSION['win'] = TRUE; ?>
-					<?php } elseif ($_SESSION['dealer']->getScore() < $_SESSION['player']->getScore()) { ?>
-						<h3>Player Wins!</h3>
-						<?php $_SESSION['win'] = TRUE; ?>
-					<?php } else { ?>
-						<h3>Dealer Wins!</h3>
-						<?php $_SESSION['win'] = TRUE; ?>
-					<?php } ?>
-				<?php } else { ?>
-					<form method="POST" action="">
-						<button class='btn-md btn-success' name="hit" type="submit" value="yes" autofocus="autofocus">Hit</button>
-						<button class='btn-md btn-danger' name="stay" type="submit" value="yes">Stay</button>
-					</form>
-				<?php } ?>
-			</div>
+		<div class='col-md-4'>
+			<h3>Dealer Hand Score: <span id="score-0">0</span></h3>
+			<div id="hand-0" class="row"></div>
+		</div>
+		<div class="col-md-4">
+			<h3>Deck Card Count: <span id="deck-count">52</span></h3>
+			<div id="deck-display" class="row"></div>
+		</div>
+	</div>
+	<div class="row">
+		
+		<div id="game-options" class="text-center">
+			<button class="btn btn-success" id="start">Start Game</button>
+			<button class="btn btn-success" id="deal">Deal Game</button>
+			<button class="btn btn-danger" id="stop">End Game</button>
 		</div>
 	</div>
 
+@stop
+
+@section('bottom-script')
+	<script type="text/javascript" src="/js/blackjack.js"></script>
+	<script src="/js/popup-buttons.js"></script>
 @stop
