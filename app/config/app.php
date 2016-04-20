d0868b06ca4994d5f2bfb10678dee28d0bb2c6d4
Cameron Holland <cameron@thenearsky.com>
2016-04-20 17:43:10 -0500

    added js for markdown editor

--- app/config/app.php
+++ app/config/app.php
@@ -107,6 +107,7 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
+		'VTalbot\Markdown\MarkdownServiceProvider',
 
 	),
 
@@ -173,6 +174,7 @@ return array(
 		'URL'             => 'Illuminate\Support\Facades\URL',
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
+		'Markdown' 		  => 'VTalbot\Markdown\Facades\Markdown',
 
 	),
 
